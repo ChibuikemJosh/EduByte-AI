bc77e859eb0f690e592ea927702ce2c664c58dd5
Egbuchiri Chibuikem Josiah <egbuchirichibuikem@gmail.com>
2026-07-03 14:41:18 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,3 @@
-import { Routes, Route } from "react-router-dom";
-
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Chat from "./pages/Chat";
-import Course from "./pages/Course";
-import Quiz from "./pages/Quiz";
-import Profile from "./pages/Profile";
-import NotFound from "./pages/NotFound";
-
 import Router from "./router";
 
 export default function App(){
